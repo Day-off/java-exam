bfb6128b5d8bf75dd8f0ca5179f11ca29865482a
krsobo <krsobo@ttu.ee>
2022-02-07 17:55:07 +0200

    web

--- PR/PR02DataStructures/src/ee/taltech/iti0202/datastructures/DataStructures.java
+++ PR/PR02DataStructures/src/ee/taltech/iti0202/datastructures/DataStructures.java
@@ -1,7 +1,15 @@
 package ee.taltech.iti0202.datastructures;
 
 
-import java.util.*;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.ArrayList;
+import java.util.Collections;
+
+
+
+
 
 public class DataStructures {
 
