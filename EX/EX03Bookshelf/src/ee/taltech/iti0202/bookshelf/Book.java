2f93cf77ec3520d3a9a7835f74beb32e873ef2db
krsobo <krsobo@ttu.ee>
2022-02-11 22:14:53 +0200

    web

--- EX/EX03Bookshelf/src/ee/taltech/iti0202/bookshelf/Book.java
+++ EX/EX03Bookshelf/src/ee/taltech/iti0202/bookshelf/Book.java
@@ -1,5 +1,6 @@
 package ee.taltech.iti0202.bookshelf;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Objects;
 
@@ -7,16 +8,23 @@ public class Book {
     private final String bName;
     private final String bAuthor;
     Person owner;
+    Book cop;
     private Integer byearOfPublishing = 0;
     private final Integer bprice;
     private Integer bId = 0;
     private static Integer nextbId = -1;
+    private static List<Book> bookList = new ArrayList<>();
 
+    /***
+     * index counter
+     */
     public static int getAndIncrementNextId() {
         return nextbId += 1 ;
     }
 
-
+    /***
+     *Book Constructor
+     */
     public Book(String title, String author, int yearOfPublishing, int price) {
         bName = title;
         bAuthor = author;
@@ -25,36 +33,60 @@ public class Book {
         bId = getAndIncrementNextId();
     }
 
+    /***
+     * Title getter
+     */
     public String getTitle() {
 
         return this.bName;
     }
 
+    /***
+     * Author getter
+     */
     public String getAuthor() {
 
         return bAuthor;
     }
 
+    /***
+     * year getter
+     */
     public int getYearOfPublishing() {
         return this.byearOfPublishing;
     }
 
+    /***
+     * owner getter
+     */
     public Person getOwner() {
         return owner;
     }
 
+    /***
+     * price getter
+     */
     public int getPrice() {
         return bprice;
     }
 
+    /***
+     * Id getter
+     */
     public int getId() {
         return bId;
     }
 
+    /***
+     * Owner setter
+     */
     public void setOwner(Person newOwner) {
         this.owner = newOwner;
     }
 
+    /***
+     * Check person can buy book
+     */
     public boolean buy(Person buyer) {
         if (this.owner == null) {
             if (buyer != null) {
@@ -69,5 +101,24 @@ public class Book {
         }
     }
 
+    public static Book of(String title, String author, int yearOfPublishing, int price){
+        return null;
+    }
+
+    public static Book of(String title, int price){
+        return null;
+    }
+
+    public static List<Book> getBooksByOwner(Person owner){
+        return null;
+    }
+
+    public static boolean removeBook(Book book){
+        return false;
+    }
+
+    public static List<Book> getBooksByAuthor(String author){
+        return bookList;
+    }
 
 }
