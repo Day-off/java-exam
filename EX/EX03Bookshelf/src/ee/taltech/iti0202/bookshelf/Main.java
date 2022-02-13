38da8ebc0dfa574e1432d232e6350c5fa117bcbb
krsobo <krsobo@ttu.ee>
2022-02-13 21:15:57 +0200

    book

--- EX/EX03Bookshelf/src/ee/taltech/iti0202/bookshelf/Main.java
+++ EX/EX03Bookshelf/src/ee/taltech/iti0202/bookshelf/Main.java
@@ -2,14 +2,16 @@ package ee.taltech.iti0202.bookshelf;
 
 public class Main {
 
-    private static final  Integer M1 = 1926;
+    private static final Integer M1 = 1926;
     private static final Integer P1 = 100;
-    private final static Integer M2 = 1976;
-    private final static Integer P2 = 200;
-    private final static Integer BP1 = 200;
-    private final static Integer BP2 = 300;
+    private static final Integer M2 = 1976;
+    private static final Integer P2 = 200;
+    private static final Integer BP1 = 200;
+    private static final Integer BP2 = 300;
 
-
+    /***
+     * test
+     */
     public static void main(String[] args) {
         Book tammsaare = new Book("Truth and Justice", "Tammsaare", M1, P1);
         Book meri = new Book("Silverwhite", "Meri", M2, P2);
@@ -20,24 +22,20 @@ public class Main {
         System.out.println(mati.buyBook(tammsaare)); // true
         System.out.println(mati.getMoney());  // 100
         System.out.println(tammsaare.getOwner().getName());
-        System.out.println(tammsaare.getPrice());// 100
+        System.out.println(tammsaare.getPrice()); // 100
 
-        System.out.print("\n");
 
         System.out.println(mati.sellBook(tammsaare)); // true
         System.out.println(mati.getMoney()); // 200
         System.out.println(tammsaare.getOwner()); // null
 
-        System.out.print("\n");
 
         System.out.println(mati.sellBook(null)); // false
 
-        System.out.println("\n");
 
         System.out.println(mati.buyBook(null)); // true
         System.out.println(mati.getMoney()); // 0
 
-        System.out.println("\n");
 
         System.out.println(meri.buy(kati)); // true
         System.out.println(mati.getMoney()); // 200`
@@ -47,8 +45,6 @@ public class Main {
         System.out.println(meri.getOwner().getName()); // Kati
         System.out.println(kati.sellBook(meri)); // true
         System.out.println(meri.getOwner()); // null
-
-        System.out.println("\n");
 
         // id
         System.out.println(tammsaare.getId()); // 0
