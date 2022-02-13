0952446a9b3ebe939d7985343f28154d7cef3931
krsobo <krsobo@ttu.ee>
2022-02-13 11:41:10 +0200

    book

--- PR/PR03Ring/src/ee/taltech/iti0202/lotr/Person.java
+++ PR/PR03Ring/src/ee/taltech/iti0202/lotr/Person.java
@@ -26,17 +26,19 @@ public class Person {
     public void check() {
         if (Objects.equals(pName, "Sauron") && pRing == null) {
             res = "No, but he's claiming to be";
-        } else if (Objects.equals(pName, "Sauron") && pRing.getType() == ee.taltech.iti0202.lotr.Ring.Type.THE_ONE
-                && pRing.getMaterial() == ee.taltech.iti0202.lotr.Ring.Material.GOLD) {
-            res = "Affirmative";
-        } else if (Objects.equals(pName, "Sauron") && pRing.getType() == ee.taltech.iti0202.lotr.Ring.Type.THE_ONE
-                && pRing.getMaterial() != ee.taltech.iti0202.lotr.Ring.Material.GOLD) {
-            res = "No, the ring is fake!";
-        } else if (!Objects.equals(pName, "Sauron") && pRing.getType() == ee.taltech.iti0202.lotr.Ring.Type.THE_ONE
-                && pRing.getMaterial() == ee.taltech.iti0202.lotr.Ring.Material.GOLD) {
-            res = "No, he just stole the ring";
-        } else if (Objects.equals(pName, "Sauron") && pRing.getType() != ee.taltech.iti0202.lotr.Ring.Type.THE_ONE) {
-            res = "No, but he's claiming to be";
+        } else if (pRing != null) {
+            if (Objects.equals(pName, "Sauron") && pRing.getType() == ee.taltech.iti0202.lotr.Ring.Type.THE_ONE
+                    && pRing.getMaterial() == ee.taltech.iti0202.lotr.Ring.Material.GOLD) {
+                res = "Affirmative";
+            } else if (Objects.equals(pName, "Sauron") && pRing.getType() == ee.taltech.iti0202.lotr.Ring.Type.THE_ONE
+                    && pRing.getMaterial() != ee.taltech.iti0202.lotr.Ring.Material.GOLD) {
+                res = "No, the ring is fake!";
+            } else if (!Objects.equals(pName, "Sauron") && pRing.getType() == ee.taltech.iti0202.lotr.Ring.Type.THE_ONE
+                    && pRing.getMaterial() == ee.taltech.iti0202.lotr.Ring.Material.GOLD) {
+                res = "No, he just stole the ring";
+            } else if (Objects.equals(pName, "Sauron") && pRing.getType() != ee.taltech.iti0202.lotr.Ring.Type.THE_ONE) {
+                res = "No, but he's claiming to be";
+            }
         } else {
             res = "No";
         }
