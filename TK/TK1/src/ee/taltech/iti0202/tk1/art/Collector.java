5dae5ccdb1b692f151f822d513ce6a6ffd9d95c9
krsobo <krsobo@ttu.ee>
2022-02-23 16:38:35 +0200

    tk

--- TK/TK1/src/ee/taltech/iti0202/tk1/art/Collector.java
+++ TK/TK1/src/ee/taltech/iti0202/tk1/art/Collector.java
@@ -2,6 +2,7 @@ package ee.taltech.iti0202.tk1.art;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Objects;
 
 public class Collector {
     private List<Painting> plist = new ArrayList<>();
@@ -11,12 +12,13 @@ public class Collector {
     }
 
     public boolean addPainting(Painting painting) {
-        if (!plist.contains(painting)) {
-            plist.add(painting);
-            return true;
-        } else {
-            return false;
+        for (Painting p : plist) {
+            if (Objects.equals(p.getTitle(), painting.getTitle())) {
+                return false;
+            }
         }
+        plist.add(painting);
+        return true;
     }
 
     public boolean sellPainting(Painting painting, Collector fellowCollector) {
@@ -30,5 +32,40 @@ public class Collector {
 
     public List<Painting> getPaintings() {
         return plist;
+    }
+
+    public static void main(String[] args) {
+
+        Painting painting1 = new Painting("The last supper", "Leonardo");
+        System.out.println(painting1);  // This is a painting named The last supper and made by Leonardo.
+        Painting painting2 = new Painting("Woman with Red Head Scarf");
+        System.out.println(painting2);  // This is a painting named Woman with Red Head Scarf and made by an unknown artist.
+
+        Painting forgery = new Painting("The last supper", "Leonerdo");
+        System.out.println(forgery);  // This is a painting named The last supper and made by Leonerdo.
+        System.out.println();
+
+        Collector collector1 = new Collector();
+        Collector collector2 = new Collector();
+
+        System.out.println(collector1.addPainting(painting1)); // true
+        System.out.println(collector1.addPainting(painting1)); // false
+        System.out.println(collector1.addPainting(painting2)); // true
+        System.out.println();
+        System.out.println(collector2.getPaintings()); // []
+        System.out.println(collector1.getPaintings());
+// [This is a painting named The last supper and made by Leonardo.,
+// This is a painting named Woman with Red Head Scarf and made by an unknown artist.]
+        System.out.println();
+        System.out.println(collector1.sellPainting(painting1, collector2)); // true
+        System.out.println(collector2.sellPainting(painting2, collector1)); // false
+        System.out.println(collector1.sellPainting(painting2, collector1)); // false
+        System.out.println();
+        System.out.println(collector1.getPaintings());
+// [This is a painting named Woman with Red Head Scarf and made by an unknown artist.]
+        System.out.println(collector2.getPaintings());
+// [This is a painting named The last supper and made by Leonardo.]
+        System.out.println(collector2.addPainting(forgery)); // false
+
     }
 }
