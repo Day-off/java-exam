578f7693796eefb76ff724c6c327ff8fc0db3982
krsobo <krsobo@ttu.ee>
2022-02-25 18:58:32 +0200

    tk2

---
+++ TK/TK2/src/ee/taltech/iti0202/tk2/shop/Shop.java
@@ -0,0 +1,46 @@
+package ee.taltech.iti0202.tk2.shop;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Objects;
+import java.util.Optional;
+
+public class Shop {
+
+    private List<Product> korv = new ArrayList<>();
+
+    public Shop() {
+
+    }
+
+    public boolean addProduct(Product product) {
+        if (product.getPrice() < 0) {
+            return false;
+        }
+        for (Product ob : korv) {
+            if (!Objects.equals(product.getPrice(), ob.getPrice()) && !Objects.equals(ob.getName(), product.getName())) {
+                korv.add(product);
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public Optional<Product> sellProduct(String name, int maxPrice) {
+        Integer pr = 0;
+        Product needed = null;
+        for (Product ob : korv) {
+            if (ob.getPrice() <= maxPrice) {
+                if (ob.getPrice() > pr) {
+                    pr = ob.getPrice();
+                    needed = ob;
+                }
+            }
+        }
+        if (needed == null) {
+            return Optional.empty();
+        } else {
+            return Optional.of(needed);
+        }
+    }
+}
