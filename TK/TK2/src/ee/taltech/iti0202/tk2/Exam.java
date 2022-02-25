5014e7594231c57b465e12c942576af7f051ab7d
krsobo <krsobo@ttu.ee>
2022-02-25 19:19:02 +0200

    tk2

--- TK/TK2/src/ee/taltech/iti0202/tk2/Exam.java
+++ TK/TK2/src/ee/taltech/iti0202/tk2/Exam.java
@@ -1,10 +1,7 @@
 package ee.taltech.iti0202.tk2;
 
-import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
 import java.util.Map;
-import java.util.stream.Collectors;
 
 public class Exam {
 
