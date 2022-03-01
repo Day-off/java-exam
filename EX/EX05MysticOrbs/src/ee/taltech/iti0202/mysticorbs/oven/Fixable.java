0c3fdf5d1ea67295984c1d7a6a5ea08b6f979a78
krsobo <krsobo@ttu.ee>
2022-03-01 12:16:49 +0200

    orbs

--- EX/EX05MysticOrbs/src/ee/taltech/iti0202/mysticorbs/oven/Fixable.java
+++ EX/EX05MysticOrbs/src/ee/taltech/iti0202/mysticorbs/oven/Fixable.java
@@ -3,6 +3,14 @@ package ee.taltech.iti0202.mysticorbs.oven;
 import ee.taltech.iti0202.mysticorbs.exceptions.CannotFixException;
 
 public interface Fixable {
+
+    /***
+     * throw error
+     */
     void fix() throws CannotFixException;
+
+    /***
+     * count time
+     */
     int getTimesFixed();
 }
