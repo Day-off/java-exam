30f9e0e51d5649fb2bfb5bee541960d206cd2edd
krsobo <krsobo@ttu.ee>
2022-03-01 12:43:57 +0200

    orbs

--- EX/EX05MysticOrbs/src/ee/taltech/iti0202/mysticorbs/exceptions/CannotFixException.java
+++ EX/EX05MysticOrbs/src/ee/taltech/iti0202/mysticorbs/exceptions/CannotFixException.java
@@ -2,9 +2,7 @@ package ee.taltech.iti0202.mysticorbs.exceptions;
 
 import ee.taltech.iti0202.mysticorbs.oven.Oven;
 
-import java.awt.desktop.UserSessionEvent;
-
-public class CannotFixException extends Exception{
+public class CannotFixException extends Exception {
 
     private final Oven brokenOven;
     private final Reason error;
@@ -15,7 +13,10 @@ public class CannotFixException extends Exception{
         NOT_ENOUGH_RESOURCES
     }
 
-    public CannotFixException(Oven oven, Reason reason){
+    /***
+     * constructor
+     */
+    public CannotFixException(Oven oven, Reason reason) {
         brokenOven = oven;
         error = reason;
     }
