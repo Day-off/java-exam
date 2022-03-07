9ce9f00f616f3d0d16289fd3c53f9c02c0e07e8e
krsobo <krsobo@ttu.ee>
2022-03-07 17:22:28 +0200

    Ex06 - 22

--- EX/EX06Files/src/ee/taltech/iti0202/files/input/FileReaderException.java
+++ EX/EX06Files/src/ee/taltech/iti0202/files/input/FileReaderException.java
@@ -2,6 +2,9 @@ package ee.taltech.iti0202.files.input;
 
 public class FileReaderException extends RuntimeException {
 
+    /***
+     * Constructor
+     */
     public FileReaderException(String message, Throwable reason){
        super(message, reason);
     }
