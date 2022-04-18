6efba962d1f99b0ab4a894c8c7623bead622ffcf
krsobo <krsobo@ttu.ee>
2022-04-18 22:36:13 +0300

    EX11 - 8 test if all needed classes exist

--- EX/EX11ComputerStore/src/ee/taltech/iti0202/computerstore/database/Database.java
+++ EX/EX11ComputerStore/src/ee/taltech/iti0202/computerstore/database/Database.java
@@ -77,9 +77,9 @@ public class Database {
     }
 
     public void saveToFile(String location) throws IOException {
+        Gson gson = new Gson();
+        List<Component> comp = components.values().stream().toList();
         try {
-            Gson gson = new Gson();
-            List<Component> comp = components.values().stream().toList();
             gson.toJson(comp, new FileWriter("components.json"));
         } catch (IOException e) {
             throw new IOException();
@@ -87,8 +87,8 @@ public class Database {
     }
 
     public void loadFromFile(String location) throws IOException {
+        Gson gson = new Gson();
         try {
-            Gson gson = new Gson();
             Reader reader = Files.newBufferedReader(Paths.get("components.json"));
             List<Component> componentsList = new Gson().fromJson(reader, new TypeToken<List<Component>>() {
             }.getType());
