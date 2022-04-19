99bab8ab1373e21e0d8d5dfa7158720e6dd0db74
krsobo <krsobo@ttu.ee>
2022-04-19 19:41:23 +0300

    EX11 - 41

--- EX/EX11ComputerStore/src/ee/taltech/iti0202/computerstore/database/Database.java
+++ EX/EX11ComputerStore/src/ee/taltech/iti0202/computerstore/database/Database.java
@@ -1,22 +1,12 @@
 package ee.taltech.iti0202.computerstore.database;
 
-import com.google.gson.Gson;
-import com.google.gson.reflect.TypeToken;
 import ee.taltech.iti0202.computerstore.components.Component;
 import ee.taltech.iti0202.computerstore.exceptions.OutOfStockException;
 import ee.taltech.iti0202.computerstore.exceptions.ProductAlreadyExistsException;
 import ee.taltech.iti0202.computerstore.exceptions.ProductNotFoundException;
 
-import java.io.FileWriter;
-import java.io.IOException;
-import java.io.Reader;
-import java.nio.file.Files;
-import java.nio.file.Paths;
 import java.util.HashMap;
-import java.util.List;
 import java.util.Map;
-import java.util.function.Function;
-import java.util.stream.Collectors;
 
 public class Database {
     private static Database instance = null;
@@ -84,25 +74,25 @@ public class Database {
     }
 
     public void saveToFile(String location) {
-        try {
-            Gson gson = new Gson();
-            List<Component> comp = components.values().stream().toList();
-            gson.toJson(comp, new FileWriter(location));
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
+//        try {
+//            Gson gson = new Gson();
+//            List<Component> comp = components.values().stream().toList();
+//            gson.toJson(comp, new FileWriter(location));
+//        } catch (IOException e) {
+//            e.printStackTrace();
+//        }
     }
 
     public void loadFromFile(String location) {
-        try {
-            Reader reader = Files.newBufferedReader(Paths.get(location));
-            List<Component> componentsList = new Gson().fromJson(reader, new TypeToken<List<Component>>() {
-            }.getType());
-            components = componentsList.stream()
-                    .collect(Collectors.toMap(Component::getId, Function.identity()));
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
+//        try {
+//            Reader reader = Files.newBufferedReader(Paths.get(location));
+//            List<Component> componentsList = new Gson().fromJson(reader, new TypeToken<List<Component>>() {
+//            }.getType());
+//            components = componentsList.stream()
+//                    .collect(Collectors.toMap(Component::getId, Function.identity()));
+//        } catch (IOException e) {
+//            e.printStackTrace();
+//        }
 
     }
 }
