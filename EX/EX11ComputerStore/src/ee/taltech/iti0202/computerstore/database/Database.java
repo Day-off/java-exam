d479fed3b9b5686aefd3fb66b0bde3ff24dcaa46
krsobo <krsobo@ttu.ee>
2022-04-19 18:09:20 +0300

    EX11 - 27

--- EX/EX11ComputerStore/src/ee/taltech/iti0202/computerstore/database/Database.java
+++ EX/EX11ComputerStore/src/ee/taltech/iti0202/computerstore/database/Database.java
@@ -34,7 +34,7 @@ public class Database {
     }
 
     public void saveComponent(Component component) throws ProductAlreadyExistsException {
-        if (!components.containsValue(component)) {
+        if (!components.containsValue(component) && !components.containsKey(component.getId())) {
             components.put(component.getId(), component);
         } else {
             throw new ProductAlreadyExistsException();
